62d0a1003ee8430f048222a05475998e451315f2
Sean Mansell <thearknano@gmail.com>
2021-07-15 19:22:22 +0930

    add naughty role mocking

--- cogs/responses.py
+++ cogs/responses.py
@@ -15,13 +15,39 @@ class ResponsesCog(commands.Cog):
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
-        for entry in self.responses:
-            for trigger in self.responses[entry]['triggers']:
-                if (trigger in message.content.lower()):
-                    if (self.responses[entry]['chance'] > random.random()):
-                        await message.channel.send(random.choice(self.responses[entry]['responses']))
-                        break
+        await naughtyMock(self, message)
+        await funResponses(self, message)
 
+
+async def naughtyMock(self, message):
+    if "naughty" in [y.name.lower() for y in message.author.roles]:
+        if message.content == "im sorry meeku i love you":
+            role = discord.utils.get(message.guild.roles, name='naughty')
+            await message.author.remove_roles(role)
+            await message.channel.send("Good.")
+        else:
+            await message.channel.send(spongemock(message.content))
+    
+
+async def funResponses(self, message):
+    for entry in self.responses:
+        for trigger in self.responses[entry]['triggers']:
+            if (trigger in message.content.lower()):
+                if (self.responses[entry]['chance'] > random.random()):
+                    await message.channel.send(random.choice(self.responses[entry]['responses']))
+                    break
 
 def setup(bot):
     bot.add_cog(ResponsesCog(bot))
+
+def spongemock(input_text):
+    output_text = ""
+    for char in input_text:
+        if char.isalpha():
+            if random.random() > 0.5:
+                output_text += char.upper()
+            else:
+                output_text += char.lower()
+        else:
+            output_text += char
+    return output_text
